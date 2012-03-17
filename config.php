19efd660c014c0eadd0951472bd281da7a866e75
Dominic Simard <dominic.simard@verteb.ca>
2012-03-17 16:09:45 -0400

    added

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+//Infos générales du site
+define('COMPANY_NAME', '');
+define('COMPANY_MOTO', '');
+define('COMPANY_ADDRESS', '');
+define('COMPANY_CITY', 'Québec');
+define('COMPANY_PROVINCE', 'QC');
+define('COMPANY_POSTAL_CODE', '');
+define('COMPANY_COUNTRY', 'Canada');
+define('COMPANY_TEL', '');
+define('COMPANY_FAX', '');
+define("COMPANY_EMAIL", "");
+define("COMPANY_GEOTAGS", ''); //http://www.geo-tag.de/generator/en.html
+define("COMPANY_TWITTER", "");
+define("COMPANY_TWITTER_NUMBER_OF_POSTS", 5);
+define("COMPANY_FACEBOOK_ID", "");
+define("COMPANY_FACEBOOK_NUMBER_OF_POSTS", 5);
+?>
