984f8afb59552c2301e5319decab5a994b00e2da
Dominic Simard <dominic.simard@verteb.ca>
2012-03-17 17:21:26 -0400

    added ie6, 7, 8 conditionnal comments classes

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ $alias = getAlias($alias);
 $isHome = isHome($alias);
 ?>
 <!doctype html>
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7 oldie" lang="fr"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8 oldie" lang="fr"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9 oldie" lang="fr"> <![endif]-->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7 ie6 oldie" lang="fr"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8 ie7 oldie" lang="fr"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9 ie8 oldie" lang="fr"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
